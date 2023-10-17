d9b63c77e7787c79a9e03f2c4dc852fab5d519aa
mohammad-elayyan <mohmmadalean1@gmail.com>
2023-10-17 11:42:02 +0300

    upload files

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ startBtn.addEventListener("click", () => {
   showHide(s2.children[0], "flex");
   showHide(s2.children[1], "flex");
 
-  resetBg("url(./assets/images/b/bottom.png)", "77vw");
+  resetBg("url(./assets/images/b/bottom.png)", "70vw");
 });
 
 home.addEventListener("click", () => {
@@ -73,12 +73,12 @@ popupsBtns.forEach((btn, btnIndx) => {
     switch (btnIndx) {
       case 0:
         btn.src = btn.src.slice(0, btn.src.lastIndexOf("/")) + "/gl-sel.svg";
-        resetBg("url(./assets/images/gloss/back/b.png)", "106vw");
+        resetBg("url(./assets/images/gloss/back/b.png)", "96.3vw");
         glossHandler("init");
         break;
       case 1:
         btn.src = btn.src.slice(0, btn.src.lastIndexOf("/")) + "/act-sel.svg";
-        resetBg("url(./assets/images/ex3/b.png)", "228vw");
+        resetBg("url(./assets/images/ex3/b.png)", "208vw");
         exHandler("init");
         break;
       case 2:
@@ -118,7 +118,7 @@ function glossHandler(action) {
           glossBtns[i].src =
             btn.src.slice(0, btn.src.lastIndexOf("/")) +
             "/" +
-            (btnIndx + 1) +
+            (i + 1) +
             "-t.svg";
         }
         btn.src =
@@ -177,7 +177,7 @@ function exHandler(action) {
 
           ex.append(ex3);
 
-          resetBg("url(./assets/images/ex3/b3.png)", "228vw");
+          resetBg("url(./assets/images/ex3/b3.png)", "208vw");
           navImg.src =
             navImg.src.slice(0, navImg.src.lastIndexOf("/")) +
             "/" +
@@ -187,118 +187,129 @@ function exHandler(action) {
           const checkBtns = bottomB.querySelectorAll(".ex3 .check");
 
           checkBtns.forEach((btn, btnIndx) => {
+            let inputs = btn.nextElementSibling.children;
             btn.onclick = () => {
-              let inputs = btn.nextElementSibling.children;
-              let res = false;
-              for (let i = 0; i < inputs.length; i++) {
-                // console.log(inputs[i].value);
-                if (i + 1 < inputs.length) {
-                  const ex3 = bottomB.querySelector(".ex3");
-                  const overlay = document.createElement("div");
-                  overlay.className = "overlay z-2";
+              if (
+                inputs[0].value !== "" &&
+                inputs[1].value !== "" &&
+                !isNaN(inputs[0].value) &&
+                !isNaN(inputs[1].value)
+              ) {
+                let res = false;
+                for (let i = 0; i < inputs.length; i++) {
+                  // console.log(inputs[i].value);
+                  if (i + 1 < inputs.length) {
+                    const ex3 = bottomB.querySelector(".ex3");
+                    const overlay = document.createElement("div");
+                    overlay.className = "overlay z-2";
 
-                  switch (btnIndx) {
-                    case 0:
-                      res = inputCheck(
-                        inputs[i].value,
-                        inputs[i + 1].value,
-                        4,
-                        2
-                      );
-                      if (res) {
-                        overlay.innerHTML = `
-                        <button onclick="removeOverlay()" style="top: 52.5vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q1t.svg" class="box z-3" style="top: 69.7vw;" alt="" />
-                        <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 51vw;" alt="" />
-                        <h1 class="z-4" style="top: 74.9vw;">${
-                          inputs[i].value / inputs[(i = 1)].value
-                        }</h1>
-                          <button class="fb-btn z-4">أحسنت</button>`;
-                      } else {
-                        overlay.innerHTML = `
-                          <button onclick="removeOverlay()" style="top: 52.5vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q1f.svg" class="box z-3" style="top: 69.7vw;" alt="" />
-                        <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 51vw;" alt="" />
-                        <h1 class="z-4" style="top: 74.9vw;color:#dc767c;">${
-                          inputs[i].value / inputs[(i = 1)].value
-                        }</h1>
-                        <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
-                        inputs[0].style.color = "#dc767c";
-                        inputs[1].style.color = "#dc767c";
-                      }
-                      inputs[0].setAttribute("readonly", "true");
-                      inputs[1].setAttribute("readonly", "true");
-                      ex3.children[btnIndx].append(overlay);
-                      break;
-                    case 1:
-                      res = inputCheck(
-                        inputs[i].value,
-                        inputs[i + 1].value,
-                        4,
-                        2
-                      );
-                      if (res) {
-                        overlay.innerHTML = `
-                        <button onclick="removeOverlay()" style="top: 91.5vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q2t.svg" class="box z-3" style="top: 109.2vw;" alt="" />
-                        <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 90vw;" alt="" />
-                        <h1 class="z-4" style="top: 114.25vw;">${
-                          inputs[i].value / inputs[(i = 1)].value
-                        }</h1>
-                          <button class="fb-btn z-4">أحسنت</button>`;
-                      } else {
-                        overlay.innerHTML = `
-                          <button onclick="removeOverlay()" style="top: 91.5vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q2f.svg" class="box z-3" style="top: 109.2vw;" alt="" />
-                        <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 90vw;" alt="" />
-                        <h1 class="z-4" style="top: 114.25vw;color:#dc767c;">${
-                          inputs[i].value / inputs[(i = 1)].value
-                        }</h1>
-                        <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
-                        inputs[0].style.color = "#dc767c";
-                        inputs[1].style.color = "#dc767c";
-                      }
-                      inputs[0].setAttribute("readonly", "true");
-                      inputs[1].setAttribute("readonly", "true");
-                      ex3.children[btnIndx].append(overlay);
-                      break;
-                    case 2:
-                      res = inputCheck(
-                        inputs[i].value,
-                        inputs[i + 1].value,
-                        6,
-                        0
-                      );
-                      if (res) {
-                        overlay.innerHTML = `
-                        <button onclick="removeOverlay()" style="top: 144.8vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q3t.svg" class="box z-3" style="top: 163.53vw;" alt="" />
-                        <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 143.5vw;" alt="" />
-                        
-                          <button class="fb-btn z-4">أحسنت</button>`;
-                      } else {
-                        overlay.innerHTML = `
-                          <button onclick="removeOverlay()" style="top: 144.8vw;" class="x z-4"></button>
-                        <img src="./assets/images/ex3/q3f.svg" class="box z-3" style="top: 163.53vw;" alt="" />
-                        <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 143.5vw;" alt="" />
-                        
-                        <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
-                        inputs[0].style.color = "#dc767c";
-                        inputs[1].style.color = "#dc767c";
-                      }
-                      inputs[0].setAttribute("readonly", "true");
-                      inputs[1].setAttribute("readonly", "true");
-                      ex3.children[btnIndx].append(overlay);
-                      break;
-                    default:
-                      break;
+                    switch (btnIndx) {
+                      case 0:
+                        res = inputCheck(
+                          inputs[i].value,
+                          inputs[i + 1].value,
+                          4,
+                          2
+                        );
+                        if (res) {
+                          overlay.innerHTML = `
+                          <button onclick="removeOverlay()" style="top: 43.5vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q1t.svg" class="box z-3" style="top: 60.15vw;" alt="" />
+                          <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 41vw;" alt="" />
+                          <h1 class="z-4" style="top: 65vw;color:#3dd15d;">${
+                            inputs[i].value / inputs[(i = 1)].value
+                          }</h1>
+                            <button class="fb-btn z-4">أحسنت</button>`;
+                          inputs[0].style.color = "#3dd15d";
+                          inputs[1].style.color = "#3dd15d";
+                        } else {
+                          overlay.innerHTML = `
+                            <button onclick="removeOverlay()" style="top: 43.5vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q1f.svg" class="box z-3" style="top: 60.15vw;" alt="" />
+                          <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 41vw;" alt="" />
+                          <h1 class="z-4" style="top: 65vw;color:#dc767c;">${
+                            inputs[i].value / inputs[(i = 1)].value
+                          }</h1>
+                          <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
+                          inputs[0].style.color = "#dc767c";
+                          inputs[1].style.color = "#dc767c";
+                        }
+                        inputs[0].setAttribute("readonly", "true");
+                        inputs[1].setAttribute("readonly", "true");
+                        ex3.children[btnIndx].append(overlay);
+                        break;
+                      case 1:
+                        res = inputCheck(
+                          inputs[i].value,
+                          inputs[i + 1].value,
+                          4,
+                          2
+                        );
+                        if (res) {
+                          overlay.innerHTML = `
+                          <button onclick="removeOverlay()" style="top: 78.3vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q2t.svg" class="box z-3" style="top: 96vw;" alt="" />
+                          <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 77vw;" alt="" />
+                          <h1 class="z-4" style="top: 100.8vw;">${
+                            inputs[i].value / inputs[(i = 1)].value
+                          }</h1>
+                            <button class="fb-btn z-4">أحسنت</button>`;
+                        } else {
+                          overlay.innerHTML = `
+                            <button onclick="removeOverlay()" style="top: 78.3vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q2f.svg" class="box z-3" style="top: 96vw;" alt="" />
+                          <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 77vw;" alt="" />
+                          <h1 class="z-4" style="top: 100.8vw;color:#dc767c;">${
+                            inputs[i].value / inputs[(i = 1)].value
+                          }</h1>
+                          <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
+                          inputs[0].style.color = "#dc767c";
+                          inputs[1].style.color = "#dc767c";
+                        }
+                        inputs[0].setAttribute("readonly", "true");
+                        inputs[1].setAttribute("readonly", "true");
+                        ex3.children[btnIndx].append(overlay);
+                        break;
+                      case 2:
+                        res = inputCheck(
+                          inputs[i].value,
+                          inputs[i + 1].value,
+                          6,
+                          0
+                        );
+                        if (res) {
+                          overlay.innerHTML = `
+                          <button onclick="removeOverlay()" style="top: 126.3vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q3t.svg" class="box z-3" style="top: 145.3vw;" alt="" />
+                          <img src="./assets/images/ex3/t.svg" class="feedback z-3" style="top: 124.9vw;" alt="" />
+                          
+                            <button class="fb-btn z-4">أحسنت</button>`;
+                        } else {
+                          overlay.innerHTML = `
+                            <button onclick="removeOverlay()" style="top: 126.3vw;" class="x z-4"></button>
+                          <img src="./assets/images/ex3/q3f.svg" class="box z-3" style="top: 145.3vw;" alt="" />
+                          <img src="./assets/images/ex3/f.svg" class="feedback z-3" style="top: 124.9vw;" alt="" />
+                          
+                          <button onclick="removeOverlay()" class="fb-btn retry z-4">إعادة المحاولة</button>`;
+                          inputs[0].style.color = "#dc767c";
+                          inputs[1].style.color = "#dc767c";
+                        }
+                        inputs[0].setAttribute("readonly", "true");
+                        inputs[1].setAttribute("readonly", "true");
+                        ex3.children[btnIndx].append(overlay);
+                        break;
+                      default:
+                        break;
+                    }
                   }
                 }
+              } else {
+                alert("يرجى ادخال قيم صحيحة");
               }
             };
           });
         } else {
-          resetBg("url(./assets/images/ex3/b.png)", "228vw");
+          resetBg("url(./assets/images/ex3/b.png)", "208vw");
 
           switch (btnIndx) {
             case 0:
